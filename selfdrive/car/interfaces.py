80e43847dbcc4d0c177f04be3ac4df54e3bd791c
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2022-05-23 11:26:40 -0700

    ci fix

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -142,7 +142,8 @@ class CarInterfaceBase(ABC):
       events.add(EventName.doorOpen)
     if cs_out.seatbeltUnlatched:
       events.add(EventName.seatbeltNotLatched)
-    if self.dragonconf.dpGearCheck and cs_out.gearShifter != GearShifter.drive and cs_out.gearShifter not in extra_gears:
+    if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
+       cs_out.gearShifter not in extra_gears):
       events.add(EventName.wrongGear)
     if cs_out.gearShifter == GearShifter.reverse:
       events.add(EventName.reverseGear)
