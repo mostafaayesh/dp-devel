3c1d8a2df957ed216f833940b6a4dfc1f47d7aa5
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2022-05-23 11:51:34 -0700

    fixes

--- selfdrive/car/honda/carcontroller.py
+++ selfdrive/car/honda/carcontroller.py
@@ -97,7 +97,7 @@ def process_hud_alert(hud_alert):
 
 HUDData = namedtuple("HUDData",
                      ["pcm_accel", "v_cruise", "car",
-                      "lanes", "fcw", "acc_alert", "steer_required", "dashed_lanes"])
+                      "lanes", "fcw", "acc_alert", "steer_required"])
 
 
 class CarController:
